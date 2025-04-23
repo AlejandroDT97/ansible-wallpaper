59269e8030c7dda3fb9512697e4f9be54bb42f26
AlejandroDT97 <alejandrodiaz97@iesamachado.org>
2025-04-23 12:37:33 +0200

    Fix DBUS for gsettings in autostart

--- set-wallpaper.sh
+++ set-wallpaper.sh
@@ -3,5 +3,8 @@
 # Fondo por defecto
 DEFAULT_WALLPAPER="/usr/share/backgrounds/warty-final-ubuntu.png"
 
-# Cambiar el fondo usando gsettings (GNOME)
+# Exportar DBUS para poder usar gsettings en el arranque
+export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id -u)/bus"
+
+# Establecer fondo con gsettings
 gsettings set org.gnome.desktop.background picture-uri "file://$DEFAULT_WALLPAPER"
